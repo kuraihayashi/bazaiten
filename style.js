951983e7b70beb323fddcf6403a0351af95e405d
kuraihayashi <wiktoria.budny747@gmail.com>
2019-10-04 12:29:44 +0200

    add javasript file

---
+++ style.js
@@ -0,0 +1,17 @@
+var inputUser=document.querySelector('.login');
+var inputKey=document.querySelector('.password');
+var user=document.querySelector('.fa-user');
+var key=document.querySelector('.fa-key');
+
+function font(){
+  user.classList.toggle('js');
+}
+
+
+
+  user.addEventListener('click', font);
+
+
+
+  
+
